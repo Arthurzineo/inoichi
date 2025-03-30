e462dcafaf8de54cd5b7a8c28e32d6e11c27b2ed
Arthur Gonçalves <78982690+Arthurzineo@users.noreply.github.com>
2025-03-30 13:49:42 -0300

    Update certificados.sh

--- certs/certificados.sh
+++ certs/certificados.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout opensearch.key -out opensearch.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /inoichi/certs/opensearch.key -out /inoichi/certs/opensearch.crt
