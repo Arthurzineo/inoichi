fa34d2be77aaeb4ab53be68ab8652a4ae7ee5049
Arthur Gonçalves <78982690+Arthurzineo@users.noreply.github.com>
2025-03-30 13:49:05 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-echo "[*] Limpando arquivos temporários da pasta /inoichi/temp..."
 rm -rf /inoichi/temp/*
 
 echo "[*] Subindo containers com Docker Compose..."
-docker compose -f /inoichi/docker-compose.yml up -d
+docker-compose -f /inoichi/docker-compose.yml up -d
 
 echo "[*] Aguardando 60 segundos para os serviços iniciarem..."
 sleep 60
 
-echo "[*] Criando índice reconhecimento_hosts no OpenSearch..."
 /inoichi/scripts/cria_index_reconhecimento.sh
 
 python3 printlogo.py
